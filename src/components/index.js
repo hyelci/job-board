561880426c36c759b21bf1d109e356c1e2f49f3f
Hacer Yelci <haceryelci@gmail.com>
2023-02-13 23:15:07 +0000

    All jobs page

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,21 @@
 import BigSidebar from "./BigSidebar";
 import FormRow from "./FormRow";
 import FormRowSelect from "./FormRowSelect";
+import Job from "./Job";
+import JobsContainer from "./JobsContainer";
 import Logo from "./Logo";
 import Navbar from "./Navbar";
+import SearchContainer from "./SearchContainer";
 import SmallSidebar from "./SmallSidebar";
 
-export { Logo, FormRow, BigSidebar, SmallSidebar, Navbar, FormRowSelect };
+export {
+  Logo,
+  FormRow,
+  BigSidebar,
+  SmallSidebar,
+  Navbar,
+  FormRowSelect,
+  JobsContainer,
+  SearchContainer,
+  Job,
+};
